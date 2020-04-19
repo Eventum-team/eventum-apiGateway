bf7cfaa67a785ae11dd08bba60566fd5d83c6a5b
icquirogac <icquirogac@unal.edu.co>
2020-04-19 15:28:32 -0500

    Without clg's

--- src/resolvers/msAPIs/comments.js
+++ src/resolvers/msAPIs/comments.js
@@ -5,8 +5,6 @@ const completeURI = `${URI}comment`;
 const getCommentsByID = async ({ eventId }) => {
   try {
     const { data } = await axios.get(`${completeURI}/${eventId}`);
-    console.log("Todos los comentarios:");
-    console.log(data);
     return data;
   } catch (error) {
     console.log(error);
@@ -16,7 +14,6 @@ const getCommentsByID = async ({ eventId }) => {
 const createComment = async ({ input }) => {
   try {
     const { data } = await axios.post(`${completeURI}`, input); // event obj structured define in resolver function
-    console.log(data);
     return data;
   } catch (error) {
     console.log(error);
@@ -26,7 +23,6 @@ const createComment = async ({ input }) => {
 const updateComment = async ({ id, input }) => {
   try {
     const { data } = await axios.put(`${completeURI}/${id}`, input); // event obj structured define in resolver function
-    console.log(data);
     return data;
   } catch (error) {
     console.log(error);
@@ -36,7 +32,6 @@ const updateComment = async ({ id, input }) => {
 const deleteComment = async ({ id }) => {
   try {
     const { data } = await axios.delete(`${completeURI}/${id}`);
-    console.log(data);
     return data;
   } catch (error) {
     console.log(error);
