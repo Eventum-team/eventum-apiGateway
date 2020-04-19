12418286ab8560558ad8469c9d82aca1aa501bdc
diarodriguezmo <33883566+diarodriguezmo@users.noreply.github.com>
2020-04-19 14:31:06 -0500

    Update auth.js

--- src/schemas/typeDef/auth.js
+++ src/schemas/typeDef/auth.js
@@ -1,21 +1,46 @@
 const authTypeDef = `
-type Auth {
+type AuthUser {
+    id: Int
+    username: String
+    detail: String
+}
+input LogRegInput {
+    username: String
+    password: String
+}
+type LoginTokens {
+    refresh: String
+    access: String
+    detail: String
+}
+input AccessToken{
+    token: String
+}
+input RefreshToken{
+    refresh: String
+}
+type NewAccessToken{
+    access: String
+    detail: String
+}`;
 
+const authMutations = `
+type Mutation {
+    addUser(input: LogRegInput!): AuthUser
+    logUser(input: LogRegInput!): LoginTokens
+    vrfTok(input: AccessToken!): String
+    refreshTok(input: RefreshToken!): NewAccessToken
 }
 `;
 
 const authQueries = `
 type Query {
-    
+    dummy: String
 }
 `;
 
-const authMutations = `
-type Mutation {
 
-}
-`;
-
+// use by express-graphql in index.js
 const auth = `
     ${authTypeDef}
     ${authQueries}
